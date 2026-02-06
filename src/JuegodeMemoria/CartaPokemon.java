afc260794e3319259cca3ff25563264fe338fa22
Emy Canahuati <emy_canahuati@unitec.edu>
2026-02-06 09:29:59 -0600

    Clase Hija: cartaPokemon

---
+++ src/JuegodeMemoria/CartaPokemon.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JuegodeMemoria;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author emyca
+ */
+public class CartaPokemon extends Carta{
+    public CartaPokemon(String idImagen, boolean cartaRevelada, ImageIcon parteFrontal, ImageIcon parteTrasera){
+        super(idImagen, cartaRevelada, parteFrontal, parteTrasera);
+    }
+    
+    
+}
