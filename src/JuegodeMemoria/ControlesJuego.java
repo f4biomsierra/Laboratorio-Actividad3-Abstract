134e5d3f3186ad0d29a66ea055aecb81b687baf1
Emy Canahuati <emy_canahuati@unitec.edu>
2026-02-06 09:07:26 -0600

    Creacion de Interfaces

---
+++ src/JuegodeMemoria/ControlesJuego.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package JuegodeMemoria;
+
+/**
+ *
+ * @author emyca
+ */
+public interface ControlesJuego {
+    void iniciarJuego();
+    void cambiarTurno();
+    void finalizarJuego();
+}
---
+++ src/JuegodeMemoria/LogicaJuego.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package JuegodeMemoria;
+
+/**
+ *
+ * @author emyca
+ */
+public interface LogicaJuego {
+    boolean verificarParejas(Carta carta1, Carta carta2);
+    boolean juegoTerminado();
+}
